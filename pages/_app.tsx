b3c66a55093362e6b5b68f596d68c3763271db1a
Ev Haus <ev@haus.gg>
2023-02-05 15:15:02 -0800

    Migrate to GA4

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,13 @@
 /* eslint-disable react/jsx-props-no-spreading */
 
 import './_app.css';
-import {initGA, logPageView} from '../utils/analytics';
-import React, {useEffect} from 'react';
 import {SITE_AUTHOR, SITE_DESC, SITE_NAME, TWITTER_USER} from '../constants/seo';
+import Analytics from '../components/Analytics';
 import type {AppProps} from 'next/app';
 import clsx from 'clsx';
 import Head from 'next/head';
 import {Lato} from '@next/font/google';
-import Router from 'next/router';
+import React from 'react';
 
 const lato = Lato({
 	subsets: ['latin'],
@@ -22,42 +21,35 @@ const latoSemi = Lato({
 	weight: '300',
 });
 
-const _app = ({Component, pageProps}: AppProps) => {
-	useEffect(() => {
-		initGA();
-		logPageView();
-		Router.events.on('routeChangeComplete', logPageView);
-	}, []);
-
-	return (
-		<>
-			<Head>
-				<title>{SITE_NAME}</title>
-				<meta charSet='utf-8' />
-				<meta content='width=device-width,initial-scale=1,shrink-to-fit=no' name='viewport' />
-				<meta content={SITE_NAME} name='application-name' />
-				<meta content={SITE_DESC} name='description' />
-				<meta content='next.js' name='generator' />
-				<meta content='General' name='rating' />
-				<meta content='#222' name='theme-color' />
-				<meta content={SITE_AUTHOR} property='article:author' />
-				<meta content={SITE_DESC} property='og:description' />
-				<meta content='en_US' property='og:locale' />
-				<meta content={SITE_NAME} property='og:site_name' />
-				<meta content={SITE_NAME} property='og:title' />
-				<meta content='website' property='og:type' />
-				<meta content='summary' property='twitter:card' />
-				<meta content={TWITTER_USER} property='twitter:creator' />
-				<meta content={SITE_DESC} property='twitter:description' />
-				<meta content={SITE_NAME} property='twitter:title' />
-				<link href='/favicon.ico' rel='icon' type='image/x-icon' />
-				<link href='https://www.google-analytics.com' rel='preconnect' />
-			</Head>
-			<div className={clsx(lato.variable, latoSemi.variable, 'fonts')}>
-				<Component {...pageProps} />
-			</div>
-		</>
-	);
-};
+const _app = ({Component, pageProps}: AppProps) => (
+	<>
+		<Head>
+			<title>{SITE_NAME}</title>
+			<meta charSet='utf-8' />
+			<meta content='width=device-width,initial-scale=1,shrink-to-fit=no' name='viewport' />
+			<meta content={SITE_NAME} name='application-name' />
+			<meta content={SITE_DESC} name='description' />
+			<meta content='next.js' name='generator' />
+			<meta content='General' name='rating' />
+			<meta content='#222' name='theme-color' />
+			<meta content={SITE_AUTHOR} property='article:author' />
+			<meta content={SITE_DESC} property='og:description' />
+			<meta content='en_US' property='og:locale' />
+			<meta content={SITE_NAME} property='og:site_name' />
+			<meta content={SITE_NAME} property='og:title' />
+			<meta content='website' property='og:type' />
+			<meta content='summary' property='twitter:card' />
+			<meta content={TWITTER_USER} property='twitter:creator' />
+			<meta content={SITE_DESC} property='twitter:description' />
+			<meta content={SITE_NAME} property='twitter:title' />
+			<link href='/favicon.ico' rel='icon' type='image/x-icon' />
+			<link href='https://www.google-analytics.com' rel='preconnect' />
+		</Head>
+		<div className={clsx(lato.variable, latoSemi.variable, 'fonts')}>
+			<Component {...pageProps} />
+			<Analytics />
+		</div>
+	</>
+);
 
 export default _app;
