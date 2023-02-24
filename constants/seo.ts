fee8bed3fe9d3da58b81a0cf3f20532676be758c
Ev Haus <ev@haus.gg>
2023-02-23 19:30:21 -0800

    Upgrade to Next 13.2

--- constants/seo.ts
+++
@@ -1,7 +0,0 @@
-export const SITE_AUTHOR = 'Globex Designs, Inc.';
-
-export const SITE_NAME = 'Globex Designs, Inc. - Vancouver Web Design & Software Development';
-
-export const SITE_DESC = 'Globex Designs provides web design, software development and media production from Vancouver, Canada.';
-
-export const TWITTER_USER = '@globex';
